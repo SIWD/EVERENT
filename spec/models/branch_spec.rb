31969d3cbd4413b74b447ed06ea6d92526757d52
augustmacke@live.de <augustmacke@live.de>
2015-02-27 17:52:37 +0100

    Added: Readme and UserStories

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -2,23 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Branch, :type => :model do
 
-
-
   context 'Branch' do
-=begin
-    it 'is not valid with the same name' do
-      branch_category = FactoryGirl.build(:branch_category)
-
-      FactoryGirl.build(:branch, name: 'Taxi', branchCategory: branch_category)
-      FactoryGirl.build(:branch, name: 'Taxi', branchCategory: branch_category).should_not be_valid
-
-
-    end
-=end
-
     it 'is not valid without a branch_category' do
       FactoryGirl.build(:branch).should_not be_valid
+    end
 
+    it 'is valid with a branch_category' do
       branch_category = FactoryGirl.build(:branch_category)
       FactoryGirl.build(:branch, branchCategory: branch_category).should be_valid
     end
