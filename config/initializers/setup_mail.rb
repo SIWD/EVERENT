2d65d5e6605c9d702a76731bf52e71ca8fca60a4
Nico Büscher <nicobuscher@Nicos-MBP.fritz.box>
2015-06-17 17:00:56 +0200

    Try to run email server

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,11 +4,11 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.raise_delivery_errors = true
 
 ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "railscasts.com",
-    :user_name            => "bueschernico@gmail.com",
-    :password             => "mnGPasswort?",
+    :address              => "partychamp.com",
+   # :port                 => 25,
+    :domain               => "partychamp.de",
+    :user_name            => "n.buescher@partychamp.de",
+    :password             => "Champ+pass-1520",
     :authentication       => "plain",
     :enable_starttls_auto => true
 }
